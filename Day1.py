4f8c018019164c94a0a37dd081fa47beed5cf902
Muhammad Taha <taha2040qureshi@gmail.com>
2023-11-24 06:01:09 -0500

    Python 100 days challange in 30 days

---
+++ Day1.py
@@ -0,0 +1,48 @@
+# how to print in python
+print('Hello World')
+
+# how to add new line in python \n(backslash n)
+print("Hello World \n Hello World in new line")
+
+# concatinating strings
+print('Hello' + ' ' + 'world!')
+
+#indentaton in very important in python
+#this code line with give error
+  # print('Hello' + ' ' + 'world!')
+#this is the error
+# IndentationError: unexpected indent
+
+# input and output(print) functions
+# print("Hello " + input("What is your name?"))
+
+#print function with input output
+# num1 = int(input())
+# num2 = int(input())
+# print (num1*num2)
+
+
+# Provide any name in the input pane below.
+# That value can be accessed using the input() function.
+# Don't put anything inside the input() function! 
+# char_len = len(input("I can calculate the number of character what ever you enter!"))
+# print(char_len)
+
+
+# There are two variables, a and b from input
+# a = input()
+# b = input()
+# # 🚨 Don't change the code above ☝️
+# ####################################
+# # Write your code below this line 👇
+# c = a
+# a = b
+# b = c
+
+# # 🚨 Don't change the code below 👇
+# print("a: " + a)
+# print("b: " + b)
+
+
+
+
